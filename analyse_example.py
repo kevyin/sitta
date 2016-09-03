dd8ed4910606e14b2e57524830437f52ff0f371c
Kevin Ying <ying.kev@gmail.com>
2016-09-03 10:22:23 +1000

    plotly and ggplot

--- analyse_example.py
+++ analyse_example.py
@@ -5,11 +5,17 @@ Created on Sun Aug 28 22:43:10 2016
 @author: kevin
 """
 #%%
+
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import plotly
+import plotly.plotly as py
+from plotly.graph_objs import *
+plotly.tools.set_credentials_file(username='kevyin', api_key='n3c33j5hac')
+from ggplot import *
 
 pd.set_option('display.mpl_style', 'default') # Make the graphs a bit prettier
-figsize(15, 5)
 
 #%%
 df_failed = pd.read_csv('data/failed_cycle123_2.txt.gz')
@@ -31,7 +37,25 @@ df_normal_byLaneCycle.boxplot()
 
 
 #%%
+# plotly
 
+
+# trace0 = Scatter(
+#     x=[1, 2, 3, 4],
+#     y=[10, 15, 13, 17]
+# )
+# trace1 = Scatter(
+#     x=[16, 12, 13, 14],
+#     y=[16, 5, 11, 9]
+# )
+# data = Data([trace0, trace1])
+#
+# py.plot(data, filename = 'basic-line')
 
 #%%
 
+# ggplot
+ggplot(diamonds, aes(x='price', color='clarity')) + \
+    geom_density() + \
+    scale_color_brewer(type='div', palette=7) + \
+    facet_wrap('cut')
