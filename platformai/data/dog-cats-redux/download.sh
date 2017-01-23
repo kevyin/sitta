54d7ebf1d129773f73a09e284af5f3622d600fbc
Kevin Ying <ying.kev@gmail.com>
2017-01-23 19:21:43 +1100

    dogcatsredux download

---
+++ platformai/data/dog-cats-redux/download.sh
@@ -0,0 +1,4 @@
+wget https://www.kaggle.com/c/dogs-vs-cats-redux-kernels-edition/download/test.zip
+wget https://www.kaggle.com/c/dogs-vs-cats-redux-kernels-edition/download/train.zip
+wget https://www.kaggle.com/c/dogs-vs-cats-redux-kernels-edition/download/sample_submission.csv
+
